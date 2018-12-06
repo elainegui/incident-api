9e4139a6f66212e93afc7b1e60ed0f5fcda8f0f6
Elaine Ottero <a00248619@student.ait.ie>
2018-12-06 22:43:20 +0000

    file git instructions added

--- src/main/java/ie/incidentapi/users/UserRegisterRepository.java
+++ src/main/java/ie/incidentapi/users/UserRegisterRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRegisterRepository extends CrudRepository<UserRegister, String> {
 	//public void addUserRegister(UserRegister userRegister);
+	
 	//public void addUserRegister(int id, String firstName, String lastName, String country") String country, @RequestParam("city") String city),
 			// @RequestParam("email") String email,);
 
